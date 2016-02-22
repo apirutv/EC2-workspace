67f3b1792f7ddc7dc1a04d669011d5f547eba395
Apirut Vanchaam <apirut@live.com>
2016-02-22 15:13:22 +0000

    commit

---
+++ kafka/read-message.sh
@@ -0,0 +1,5 @@
+KAFKA=$HOME/kafka/kafka_2.11-0.9.0.1/
+$KAFKA/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
+
+
+
---
+++ kafka/send-message.sh
@@ -0,0 +1,4 @@
+KAFKA=$HOME/kafka/kafka_2.11-0.9.0.1/
+$KAFKA/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+
+
