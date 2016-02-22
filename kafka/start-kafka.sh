3651f3371c421a566e017a5b09a3169708bbd749
Apirut Vanchaam <apirut@live.com>
2016-02-22 14:58:59 +0000

    commit

---
+++ kafka/start-kafka.sh
@@ -0,0 +1,2 @@
+KAFKA=$HOME/kafka/kafka_2.11-0.9.0.1/
+$KAFKA/bin/kafka-server-start.sh $KAFKA/config/server.properties
---
+++ kafka/start-zookeeper.sh
@@ -0,0 +1,2 @@
+KAFKA=$HOME/kafka/kafka_2.11-0.9.0.1/
+$KAFKA/bin/zookeeper-server-start.sh $KAFKA/config/zookeeper.properties
