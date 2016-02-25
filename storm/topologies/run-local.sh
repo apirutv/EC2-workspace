95bcb81a09f7ea74cbc6df8a69ee64c47e64612c
Apirut Vanchaam <apirut@live.com>
2016-02-25 07:19:23 +0000

    commit all changes

---
+++ storm/topologies/run-local.sh
@@ -0,0 +1,5 @@
+# run using local cluster simulator
+/home/ubuntu/storm/apache-storm-0.10.0/bin/storm jar storm-example-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.learningstorm.storm_example.LearningStormTopology LearningStormTopology true
+
+# run on remote cluster
+#/home/ubuntu/storm/apache-storm-0.10.0/bin/storm jar storm-example-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.learningstorm.storm_example.LearningStormTopology LearningStormTopology false
